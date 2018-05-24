1488043c6d9ddf805f7b7df114c046b555f89159
yuzhishui <yuzhishui@outlook.com>
2018-05-24 20:59:44 +0800

    主要是设置上传图片的代码以及部分代码编写和相关设置，数据库迁移
    设置media路径的网址

--- personal_web/models.py
+++ personal_web/models.py
@@ -4,4 +4,9 @@ from django.db import models
 
 
 class personal(models.Model):
-    description = models.CharField(max_length=100)#最大限制字数
+    description = models.CharField(default='在这里写作品的简介',max_length=100)#最大限制字数
+    image = models.ImageField(default='default.png',upload_to='images/')
+    title = models.CharField(default='作品标题',max_length=50)
+
+    def __str__(self):
+        return self.title
